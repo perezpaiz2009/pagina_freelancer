8e52ef262302dbda6bc1e6873c6abeb5c5bf1763
perezpaiz2009 <perezpaiz.2009@gmail.com>
2022-09-13 22:03:53 -0600

    Libreria js animaciones añadida

---
+++ js/index.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal();
+sr.reveal('.scrollrevtop', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-100px'
+});
+
+sr.reveal('.scrollrevright', {
+    duration: 3500,
+    origin: 'right',
+    distance: '-100px'
+});
+
+sr.reveal('.scrollrevleft', {
+    duration: 3500,
+    origin: 'left',
+    distance: '-100px'
+});
+
+
+
